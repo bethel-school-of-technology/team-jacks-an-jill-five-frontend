0963ffe04a38790e9f2f500a94fc8fd64a20b522
Jbetrand10 <Joshua.betrand@gmail.com>
2023-03-05 19:52:54 -0500

    Updated the Add fair page and also created a route in the app.js file to access the form and all I created a addfair.css file for styling.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Fairlist from './components/Fairlist'
 import AboutUs from './components/AboutUs';
 import Footer from './components/Footer';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AddFair from './components/AddFair';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
    <Route path="signup" element={<SignUp />} />
    <Route path="signin" element={<SignIn />} />
    <Route path="fairlist" element={<Fairlist />} />
+   <Route path="addfair" element={<AddFair />} />
    <Route path="about" element={<AboutUs />} />
    </Route>
 
