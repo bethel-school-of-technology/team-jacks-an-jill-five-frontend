507e7502f8da68d963fac116bd45873b547c2e5b
Kyle Hammersmith <kylehammersmith@Kyles-MacBook-Pro.local>
2023-03-13 19:31:06 -0400

    changes during zoom meeting

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Fairlist from './components/Fairlist'
 import AboutUs from './components/AboutUs';
 import Footer from './components/Footer';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AddFair from './components/NewFair';
 import NewFair from './components/NewFair';
 import { UserProvider } from './contexts/UserProvider';
 import { FairProvider } from './contexts/FairProvider';
@@ -21,8 +22,8 @@ function App() {
           <Route index element={<Welcome />}/>
           <Route path="signup" element={<SignUp />} />
           <Route path="signin" element={<SignIn />} />
-          <Route path="fairlist/newfair" element={<NewFair />} />
           <Route path="fairlist" element={<Fairlist />} />
+          <Route path="addfair" element={<AddFair />} />
           <Route path="about" element={<AboutUs />} />
       </Route>
       </Routes>
