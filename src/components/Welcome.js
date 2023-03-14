b39b402f9b86840d108bf3bc2316a77474d2d1d0
hahenson <106998682+hahenson@users.noreply.github.com>
2023-03-13 20:07:28 -0400

    Upadated welcome and app.css

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,23 +1,73 @@
-import React from "react"
-import '../App.css';
-import Phone from '../Phone.png'
+import React from "react";
+import "../App.css";
+import Phone from "../Phone.png";
+import { Container } from "react-bootstrap";
 
 function Welcome() {
   return (
-    <div class='row'>
-    <div class="col-lg-8">
-      <h1>Welcome to What's Fair</h1>
-      <em><p>A unique tool to leave honest fair candid reviews on your latest trip. So that others may know about it</p></em>
-      <a href="http://localhost:3001/signup">
-      <button className="WelcomeBtn" type="button">Sign Up</button>
-      </a>
-      <em><p>Already a member<a href="http://localhost:3001/SignIn"> Click Here</a></p></em>
+    <Container fluid>
+      <div class="row">
+        <div class="col-lg-8">
+          <h1>What's Fair</h1>
+          <em>
+            <p style={{ fontSize: 20 }}>
+              An unique exciting tool that lets you leave honest fair candid{" "}
+              reviews on your latest trip. So that others may know about it.
+            </p>
+          </em>
+          <p>
+            Festivals? State Parks? State Fairs? Are you a foodie? If you can go
+            to it. You can leave a fair review. So what are you waiting
+            for......
+          </p>
+          <p>
+            Not comfrotable leaving a reveiw yet? No worries. See what others
+            are saying about events happening near you and go enjoy a wonderful
+            day.
+          </p>
+          <a href="http://localhost:3000/signup">
+            <button className="WelcomeBtn" type="button">
+              Sign Up
+            </button>
+          </a>
+          <em>
+            <p>
+              Already a member
+              <a href="http://localhost:3000/SignIn">Click Here</a>
+            </p>
+          </em>
+          <div className="WeatherAPI">
+            <h4>WEATHER API GOES HERE</h4>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <img className="Phone" src={Phone} height={500} alt="iphone-mockup" />
+        </div>
+        <div className="itemBox">
+          <em>
+            <h2 style={{ marginBottom: 40 }}>
+              3 Simple Steps to Enjoy What's Fair
+            </h2>
+          </em>
+
+          <div class="row">
+            <div class="col-lg-4">
+              <span className="emoji">📍</span>
+              <h3>Attend Event</h3>
+            </div>
+            <div class="col-lg-4">
+              <span className="emoji">😁</span>
+              <h3>Try to Enjoy</h3>
+            </div>
+            <div class="col-lg-4">
+              <span className="emoji">✍️</span>
+              <h3>Leave A Fair Review</h3>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="col-lg-4" >
-      <img className="Phone" src={Phone} height={500} alt="iphone-mockup"/>
-    </div>
-    </div>
-  )
+    </Container>
+  );
 }
 
-export default Welcome
+export default Welcome;
