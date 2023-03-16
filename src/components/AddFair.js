0f44e653f5d55a7c22c121a1ad33e041b0f340ec
hahenson <106998682+hahenson@users.noreply.github.com>
2023-03-15 21:16:33 -0400

    Upated Nav Links

--- src/components/AddFair.js
+++ src/components/AddFair.js
@@ -1,84 +1,110 @@
 import React, { useContext, useEffect } from "react";
 import { useState } from "react";
 import { Button } from "react-bootstrap";
-import Form from 'react-bootstrap/Form'
+import Form from "react-bootstrap/Form";
 import { useNavigate, useParams } from "react-router-dom";
-import '../components/AddFair.css'
+import "../components/AddFair.css";
 import FairContext from "../contexts/FairContext";
 
-function AddFair (){
+function AddFair() {
+  let navigate = useNavigate();
+  let params = useParams();
 
-    let navigate = useNavigate()
-    let params = useParams()
+  let [fair, setFair] = useState({
+    id: params.fairId,
+    fairName: "",
+    location: "",
+    description: "",
+    imageUrl: "",
+  });
 
-    let [ fair, setFair] = useState({
-        id: params.fairId,
-        fairName: "",
-        location: "",
-        description: "",
-        imageUrl: ""
-    })
+  let { getFair, createFair, updateFair } = useContext(FairContext);
+  let { id, fairName, location, description, imageUrl } = fair;
 
-    let  {getFair, createFair, updateFair} = useContext(FairContext)
-    let { id, fairName, location, description, imageUrl} = fair
-
-    useEffect(() => {
-        if (id === undefined) return
-        async function fetch() {
-            await getFair(id)
-            .then((fair) => setFair(fair))
-        }
-        fetch()
-    })
-
-    function handleChange(e) {
-        setFair((preValue) =>{
-            return { ...preValue, [e.target.name]: e.target.value}
-        })
+  useEffect(() => {
+    if (id === undefined) return;
+    async function fetch() {
+      await getFair(id).then((fair) => setFair(fair));
     }
+    fetch();
+  });
 
-    function add() {
-        if (id === undefined) {
-            return createFair(fair)
-        } else {
-            return updateFair(fair)
-        }
+  function handleChange(e) {
+    setFair((preValue) => {
+      return { ...preValue, [e.target.name]: e.target.value };
+    });
+  }
+
+  function add() {
+    if (id === undefined) {
+      return createFair(fair);
+    } else {
+      return updateFair(fair);
     }
+  }
 
-    function onSubmit(e) {
-        e.preventDefault()
-        add().then((fair) =>    
-            navigate(`/fairlist/${fair.id}`)
-        )
-    }
+  function onSubmit(e) {
+    e.preventDefault();
+    add().then((fair) => navigate(`/fairlist/${fair.id}`));
+  }
 
-    return(
-        <div className="wrap-container">
-            <div className="form-container">
-                <h1>Add Event</h1>
-            <Form onSubmit={onSubmit}>
-                <Form.Group classname= "mb-3">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control placeholder="Event Name Here" type="text" value={fairName} name="fairName" onChange={handleChange}  />
-                </Form.Group>
-                <Form.Group classname= "mb-3">
-                    <Form.Label>Decription</Form.Label>
-                    <Form.Control placeholder="Enter Description Here" type="text" value={description} name="description"  onChange={handleChange}/>
-                </Form.Group>
-                <Form.Group classname= "mb-3">
-                    <Form.Label>Location</Form.Label>
-                    <Form.Control placeholder="Enter Location Here" type="text" value={location} name="location" onChange={handleChange} />
-                </Form.Group>
-                <Form.Group classname= "mb-3">
-                    <Form.Label>Image</Form.Label>
-                    <Form.Control placeholder="Enter Image URL Here" type="text" value={imageUrl} name="imageUrl"  onChange={handleChange} />
-                </Form.Group>
+  return (
+    <div className="wrap-container">
+      <div className="form-container">
+        <h1>Add Event</h1>
+        <Form onSubmit={onSubmit}>
+          <Form.Group classname="mb-3">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              placeholder="Event Name Here"
+              type="text"
+              value={fairName}
+              name="fairName"
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Form.Group classname="mb-3">
+            <Form.Label>Decription</Form.Label>
+            <Form.Control
+              placeholder="Enter Description Here"
+              type="text"
+              value={description}
+              name="description"
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Form.Group classname="mb-3">
+            <Form.Label>Location</Form.Label>
+            <Form.Control
+              placeholder="Enter Location Here"
+              type="text"
+              value={location}
+              name="location"
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Form.Group classname="mb-3">
+            <Form.Label>Image</Form.Label>
+            <Form.Control
+              placeholder="Enter Image URL Here"
+              type="text"
+              value={imageUrl}
+              name="imageUrl"
+              onChange={handleChange}
+            />
+          </Form.Group>
 
-                <Button className="btn" variant="warning" type="submit" onClick={onSubmit}>Add Fair Here</Button>
-            </Form>
-            </div>
-        </div>
-      
-    );
-};
+          <Button
+            className="btn"
+            variant="warning"
+            type="submit"
+            onClick={onSubmit}
+          >
+            Add Fair Here
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
+}
 export default AddFair;
