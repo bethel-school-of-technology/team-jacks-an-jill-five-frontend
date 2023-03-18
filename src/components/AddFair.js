30494c64a2dead5d4b01e8f9c50ce26f16d9b939
Jbetrand10 <Joshua.betrand@gmail.com>
2023-03-17 21:12:36 -0400

    This commit doesn't need to be merged, fixing git pull issues on my local machine

--- src/components/AddFair.js
+++ src/components/AddFair.js
@@ -9,27 +9,30 @@ import FairContext from "../contexts/FairContext";
 function AddFair (){
 
     let navigate = useNavigate()
-    let params = useParams()
+    // let params = useParams()
 
     let [ fair, setFair] = useState({
-        id: params.fairId,
-        fairName: "",
-        location: "",
-        description: "",
+        fairTitle: "",
+        fairCity: "",
+        fairState: "",
+        fairStartDate: "",
+        fairEndDate: "",
+        fairDescription: "",
         imageUrl: ""
     })
 
-    let  {getFair, createFair, updateFair} = useContext(FairContext)
-    let { id, fairName, location, description, imageUrl} = fair
 
-    useEffect(() => {
-        if (id === undefined) return
-        async function fetch() {
-            await getFair(id)
-            .then((fair) => setFair(fair))
-        }
-        fetch()
-    })
+    let  { createFair} = useContext(FairContext)
+    // let { id, fairTitle, fairCity, fairState, fairDescription, fairStartDate, fairEndDate, imageUrl} = fair
+
+    // useEffect(() => {
+    //     if (fairId === undefined) return
+    //     async function fetch() {
+    //         await getFair(id)
+    //         .then((fair) => setFair(fair))
+    //     }
+    //     fetch()
+    // }, [fairId])
 
     function handleChange(e) {
         setFair((preValue) =>{
@@ -38,18 +41,24 @@ function AddFair (){
     }
 
     function add() {
-        if (id === undefined) {
+        if (fair.fairTitle !== "" && fair.fairCity !== "" && fair.fairState !== "" && fair.fairStartDate !== "" && fair.fairEndDate !== "" ) {
             return createFair(fair)
         } else {
-            return updateFair(fair)
+            alert('You need to fill all fields except description & Image')
         }
-    }
+    } 
 
     function onSubmit(e) {
         e.preventDefault()
-        add().then((fair) =>    
-            navigate(`/fairlist/${fair.id}`)
-        )
+        add(fair).then(() =>  {
+            navigate('/fairlist')
+        }).catch(error => {
+            if (error.response.status == 401) {
+                navigate('')
+            }
+            console.log(error);
+            navigate('/signin');
+        })
     }
 
     return(
@@ -59,19 +68,31 @@ function AddFair (){
             <Form onSubmit={onSubmit}>
                 <Form.Group classname= "mb-3">
                     <Form.Label>Name</Form.Label>
-                    <Form.Control placeholder="Event Name Here" type="text" value={fairName} name="fairName" onChange={handleChange}  />
+                    <Form.Control placeholder="Event Name Here" type="text" value={fair.fairTitle} name="fairTitle" onChange={handleChange}  />
                 </Form.Group>
                 <Form.Group classname= "mb-3">
                     <Form.Label>Decription</Form.Label>
-                    <Form.Control placeholder="Enter Description Here" type="text" value={description} name="description"  onChange={handleChange}/>
+                    <Form.Control placeholder="Enter Description Here" type="text" value={fair.fairDescription} name="fairDescription"  onChange={handleChange}/>
                 </Form.Group>
                 <Form.Group classname= "mb-3">
-                    <Form.Label>Location</Form.Label>
-                    <Form.Control placeholder="Enter Location Here" type="text" value={location} name="location" onChange={handleChange} />
+                    <Form.Label>City</Form.Label>
+                    <Form.Control placeholder="Enter City Here" type="text" value={fair.fairCity} name="fairCity" onChange={handleChange} />
+                </Form.Group>
+                <Form.Group classname= "mb-3">
+                    <Form.Label>State</Form.Label>
+                    <Form.Control placeholder="Enter State Here" type="text" value={ fair.fairState} name="fairState" onChange={handleChange} />
+                </Form.Group>
+                <Form.Group classname= "mb-3">
+                    <Form.Label>State Date</Form.Label>
+                    <Form.Control placeholder="Enter Date Here" type="text" value={fair.fairStartDate} name="fairStartDate" onChange={handleChange} />
+                </Form.Group>
+                <Form.Group classname= "mb-3">
+                    <Form.Label>End Date</Form.Label>
+                    <Form.Control placeholder="Enter Date Here" type="text" value={fair.fairEndDate} name="fairEndDate" onChange={handleChange} />
                 </Form.Group>
                 <Form.Group classname= "mb-3">
                     <Form.Label>Image</Form.Label>
-                    <Form.Control placeholder="Enter Image URL Here" type="text" value={imageUrl} name="imageUrl"  onChange={handleChange} />
+                    <Form.Control placeholder="Enter Image URL Here" type="text" value={fair.imageUrl} name="imageUrl"  onChange={handleChange} />
                 </Form.Group>
 
                 <Button className="btn" variant="warning" type="submit" onClick={onSubmit}>Add Fair Here</Button>
