2e95c2986ea04241054f31146f8d64dc408e1a20
Jbetrand10 <Joshua.betrand@gmail.com>
2023-03-18 09:38:33 -0400

    Start of the fairlist code not ready to be meraged yet

--- src/components/Fairlist.js
+++ src/components/Fairlist.js
@@ -1,10 +1,39 @@
 import React from "react";
+import { ListGroup } from "react-bootstrap/ListGroup";
+import FairContext from "../contexts/FairContext";
+import { Link, Outlet } from "react-router-dom";
+import { Stack } from "react-bootstrap";
 
 const Fairlist = () => {
+
+  function list() {
+    return fair.map((f) => {
+      <ListGroup.Item key={f.fairId}>
+        <div>
+          <h2>{f.fairTitle} </h2>
+          <p>{f.fairDescription}</p>
+          <Link to={`/fairs/${f.fairId}`} key={f.fairId} >
+          {f.fairState}
+        </Link>
+
+        </div>
+      </ListGroup.Item>
+    })
+  }
 
   return(
       <div>
         <h1>List of Fairs</h1>
+        <Stack direction="horizontal" gap={3}>
+        <ListGroup className="align-self-start w-75">
+          <FairContext.Consumer>
+            {({fair}) => (
+              list(fair)
+            )}
+          </FairContext.Consumer>
+        </ListGroup>
+        <Outlet />
+        </Stack>
       </div>
     );
   }
