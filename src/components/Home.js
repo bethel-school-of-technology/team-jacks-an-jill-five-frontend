189b043a8f135ecec3bea5fd9a30dcc66fc0f142
hahenson <106998682+hahenson@users.noreply.github.com>
2023-03-20 13:13:11 -0400

    Updated Items on Welcome Page with working API

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,46 @@
 import React, { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import BlackLogo from "../BlackLogo.png";
-import { Stack, Container, Navbar, Nav } from 'react-bootstrap';
-
+import { Stack, Container, Navbar, Nav } from "react-bootstrap";
 
 function Home() {
-
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
   return (
     <>
       <Navbar collapseOnSelect expand="lg" bg="warning">
         <Container>
           <Navbar.Brand href="/">
-            <img className="navbarBrand" src={BlackLogo} height={65} alt="logo" />
+            <img
+              className="navbarBrand"
+              src={BlackLogo}
+              height={65}
+              alt="logo"
+            />
             {/* link to "home" / */}
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+          <Navbar.Collapse
+            id="responsive-navbar-nav"
+            className="justify-content-end"
+          >
             <Nav>
               {/* <Link to="/" className="nav-link">Home</Link> */}
-              <Link to="/signup" className="nav-link">Sign Up</Link>
-              <Link to="/signin" className="nav-link">Sign In</Link>
-              <Link to="/addfair" className="nav-link">Add Fair</Link>
-              <Link to="/fairlist" className="nav-link">Fair List</Link>
-              <Link to="/about" className="nav-link">About Us</Link>
+              <Link to="/signup" className="nav-link">
+                Sign Up
+              </Link>
+              <Link to="/signin" className="nav-link">
+                Sign In
+              </Link>
+              <Link to="/addfair" className="nav-link">
+                Add Fair
+              </Link>
+              <Link to="/fairlist" className="nav-link">
+                Fair List
+              </Link>
+              <Link to="/about" className="nav-link">
+                About Us
+              </Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
