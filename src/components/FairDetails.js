e491089ee537cf4ceeb965a9eaa68e3469d011f6
Christina Foster <107225288+chopefoster@users.noreply.github.com>
2023-03-28 12:34:51 -0400

    added fair details with image and some style -- still working on design but committing current progress

--- src/components/FairDetails.js
+++ src/components/FairDetails.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useContext } from "react";
 import FairContext from "../contexts/FairContext";
 import { useParams, useNavigate } from 'react-router-dom';
-import { Card, Spinner } from "react-bootstrap";
+import { Card, Container, Spinner } from "react-bootstrap";
 
 const FairDetails = () => {
 
@@ -22,27 +22,64 @@ const FairDetails = () => {
         fairImage: "",
         fairWebsite: ""
     });
-    
+
     useEffect(() => {
         async function fetch() {
             await getFair(params.fairId).then((fair) => setFair(fair))
         }
         fetch()
     }, [params.fairId]);
-    
-    return (
 
-        <Card className="align-self-start w-25">
-            <Card.Img variant="top" src={fair.fairImage} />
-            <Card.Body>
-                <Card.Title>{fair.fairTitle}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">{fair.fairCity}, {fair.fairState}</Card.Subtitle>
-                <Card.Text>
-                    <strong>Dates:</strong> <span>{fair.fairStartDate} - {fair.fairEndDate}</span>
-                    <p>{fair.fairDescription}</p>
-                </Card.Text>
-            </Card.Body>
-        </Card>
+    return (
+        <><div class="container-fluid padding">
+            <div class="row text-center padding">
+                <div class="col-12">
+                    <h1 class="display-4">{fair.fairTitle}</h1>
+                </div>
+            </div>
+        </div>
+            <div class="container-fluid padding ">
+                <div class="row padding">
+                    <div class="col-lg-6 text-center">
+                        <h4>{fair.fairCity}, {fair.fairState}</h4>
+                    </div>
+                    <div class="col-lg-6 text-center">
+                        <h4>{fair.fairStartDate} - {fair.fairEndDate}</h4>
+                    </div>
+                </div>
+                <div class="container-fluid py-3">
+                    <div class="row padding">
+                        <div class="col-lg-5">
+                            <img src={fair.fairImage} className="fimage" class="img-fluid" alt="fair" />
+                        </div>
+                        <div class="col-lg-7">
+                            <p className="fairDesc" class="ml-2 py-2">{fair.fairDescription}</p>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+            <div class="banner">
+                <div class="container-fluid padding">
+                    <div class="row">
+                        <div class="col-lg-6 text-center">
+                            <h3 className="fairComment">Comments</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="container-fluid py-3" className="commentContainer">
+                    <div class="row py-1">
+                        <div class="col-12 mb-2 py-2">
+                            <p>Fair Comments will go here</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <button class="btn btn-lg">Add Comment</button>
+            </div>
+        </>
     )
 }
 
