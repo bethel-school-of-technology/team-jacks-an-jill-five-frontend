81386109de799816311ae8986e49f0a0f29a0c90
Jbetrand10 <Joshua.betrand@gmail.com>
2023-03-29 04:06:10 -0400

    Created the editfair component also updated the fairlist with the needed changes and also updated the error messages on the addfair page

--- src/components/Fairlist.js
+++ src/components/Fairlist.js
@@ -2,9 +2,12 @@ import React from "react";
 import ListGroup  from "react-bootstrap/ListGroup";
 import FairContext from "../contexts/FairContext";
 import { Link, Outlet } from "react-router-dom";
-import { Button, Card, Col, Container, Row, Stack } from "react-bootstrap";
+import { Button, Card, CardImg, Col, Container, Row, Stack } from "react-bootstrap";
 
 const Fairlist = () => {
+
+
+
   return (
     <FairContext.Consumer>
     {
@@ -17,21 +20,22 @@ const Fairlist = () => {
               <div> 
                 <Container fluid>
                   <Row className="justify-content-md-center"  >
-                  {fair.map((f) => {
+                  {fair.map((f, index) => {
                     return (
                         <Col  sm="auto" >
-                          <Card key={f.fairId} style={{ width: '18rem' }}>
+                          <Card key={f.fairId} style={{ width: '18rem' }} onClick={`/fairdetails/${f.fairId}`}>
                             <Card.Body>
-                            <Card.Title><h2>{f.fairTitle}</h2></Card.Title>
-                          <Card.Text>
+                            <CardImg className="img-thumbnail" variant="top" src={f.fairImage}/>
+                            <Card.Title onClick={`/fairdetails/${f.fairId}`}>
+                              <h2>{index+1}. {f.fairTitle}</h2>                             
+                            </Card.Title>
+                            <Card.Text>
                             <div>
-                                <h4>{f.fairCity}</h4>
-                                <p>{f.fairStartDate}</p>
+                                <h5>{f.fairCity},{f.fairState}</h5>
+                                <p>{f.fairStartDate}-{f.fairEndDate}</p>
                             </div>
                             </Card.Text>
-
                             <Link to={`/fairdetails/${f.fairId}`} className="btn btn-primary mx-1">Details</Link>
-
                             </Card.Body>
                           </Card>
                         </Col>
